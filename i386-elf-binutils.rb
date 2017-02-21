d2c67918575474115a2af4e92d0dfa3b83c24f94
wind2412 <944797358@qq.com>
2017-02-21 08:19:13 +0800

    change to the latest version!

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz'
   sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
 
   depends_on 'gcc49' => :build
