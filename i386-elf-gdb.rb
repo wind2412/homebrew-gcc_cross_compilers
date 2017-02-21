829b96cf26ebb940e4831f581c59a1be6ce42446
wind2412 <944797358@qq.com>
2017-02-21 08:18:23 +0800

    Update i386-elf-gdb.rb

--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class I386ElfGdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.xz'
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.12.tar.xz'
   sha256 '605954d5747d5f08ea4b7f48e958d1ebbf39265e18f7f36738deeabb83744485'
 
   depends_on 'i386-elf-binutils'
