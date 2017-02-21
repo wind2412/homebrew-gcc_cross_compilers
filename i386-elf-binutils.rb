e22bc3c9eadbfc50c6c4b93543d412028565d2a5
wind2412 <944797358@qq.com>
2017-02-21 08:24:43 +0800

    升级到i386-elf-gcc最新版。添加了sha256。

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -3,7 +3,7 @@ require 'formula'
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz'
-  sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
+  sha256 '26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f'
 
   depends_on 'gcc49' => :build
 
